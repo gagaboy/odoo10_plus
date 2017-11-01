69b6d7d626e8e4176860f2d5defcab224cce69ff
Kunjal <kunjal@techreceptives.com>
2017-11-01 15:38:02 +0530

    [ADD] : Branch Details and Demo data changes.

--- flectra/addons/base/res/res_branch.py
+++ flectra/addons/base/res/res_branch.py
@@ -9,7 +9,29 @@ class ResBranch(models.Model):
     name = fields.Char(string='Name', required=True)
     code = fields.Char(string='Code', required=True)
     active = fields.Boolean(string='Active', default=True)
-    partner_id = fields.Many2one('res.partner', string='Partner')
+    partner_id = fields.Many2one('res.partner', string='Partner',
+                                 ondelete='restrict')
     company_id = fields.Many2one(
         'res.company', string="Company",
         default=lambda self: self.env.user.company_id, required=True)
+    street = fields.Char()
+    street2 = fields.Char()
+    zip = fields.Char(change_default=True)
+    city = fields.Char()
+    state_id = fields.Many2one("res.country.state", string='State',
+                               ondelete='restrict')
+    country_id = fields.Many2one('res.country', string='Country',
+                                 ondelete='restrict')
+    email = fields.Char()
+    phone = fields.Char()
+    mobile = fields.Char()
+
+    _sql_constraints = [('branch_code_company_uniq', 'unique (code,company_id)',
+                         'The branch code must be unique per company!')]
+
+    @api.model
+    def create(self, vals):
+        partner_id = self.env['res.partner'].create({'name': vals['name']})
+        vals.update({'partner_id': partner_id.id})
+        res = super(ResBranch, self).create(vals)
+        return res
