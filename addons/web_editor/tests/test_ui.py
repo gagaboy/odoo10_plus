4680c4a119b4105dcfb06593828855095d53d425
Parthiv Patel <parthiv@techreceptives.com>
2017-12-15 03:43:40 -0800

    [FIX] disabled test case which was for old UI

--- addons/web_editor/tests/test_ui.py
+++ addons/web_editor/tests/test_ui.py
@@ -10,8 +10,8 @@ class TestUi(flectra.tests.HttpCase):
     post_install = True
     at_install = False
 
-    def test_01_admin_rte(self):
-        self.phantom_js("/web", "flectra.__DEBUG__.services['web_tour.tour'].run('rte')", "flectra.__DEBUG__.services['web_tour.tour'].tours.rte.ready", login='admin')
-
-    def test_02_admin_rte_inline(self):
-        self.phantom_js("/web", "flectra.__DEBUG__.services['web_tour.tour'].run('rte_inline')", "flectra.__DEBUG__.services['web_tour.tour'].tours.rte_inline.ready", login='admin')
+#     def test_01_admin_rte(self):
+#         self.phantom_js("/web", "flectra.__DEBUG__.services['web_tour.tour'].run('rte')", "flectra.__DEBUG__.services['web_tour.tour'].tours.rte.ready", login='admin')
+# 
+#     def test_02_admin_rte_inline(self):
+#         self.phantom_js("/web", "flectra.__DEBUG__.services['web_tour.tour'].run('rte_inline')", "flectra.__DEBUG__.services['web_tour.tour'].tours.rte_inline.ready", login='admin')
