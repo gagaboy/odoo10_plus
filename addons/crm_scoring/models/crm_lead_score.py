ericdai/flectra

[ADD] Lead Scoring module

addons/crm_scoring/models/crm_lead_score.py
@@ -1,0 +1,104 @@
+# Part of Flectra. See LICENSE file for full copyright and licensing details.
+
+from flectra import api, fields, models, _
+from flectra.exceptions import ValidationError
+
+
+class CRMLeadScore(models.Model):
+    _name = 'crm.lead.score'
+    _inherit = ['mail.thread']
+    _description = "Lead Soring Rule"
+
+    TYPES = {
+        'contact_type': 'Contact Type',
+        'title': 'Title',
+        'sales_channels': 'Sales Channels',
+        'contact_status': 'Contact Status',
+        'country': 'Country',
+    }
+
+    @api.model
+    def create(self, vals):
+        res = super(CRMLeadScore, self).create(vals)
+        same_records = res.search([
+            ('id', '!=', res.id),
+            ('profile_scoring_type', '=', res.profile_scoring_type)
+        ])
+        if same_records:
+            raise ValidationError(_(
+                "There's already one rule defined for '%s' type!") %
+                str(self.TYPES[res.profile_scoring_type]))
+        return res
+
+    @api.multi
+    def write(self, vals):
+        res = super(CRMLeadScore, self).write(vals)
+        if 'profile_scoring_type' in vals:
+            same_records = self.search([
+                ('id', '!=', self.id),
+                ('profile_scoring_type', '=', vals['profile_scoring_type'])
+            ])
+            if same_records:
+                raise ValidationError(_(
+                    "There's already one rule defined for '%s' type!") %
+                    str(self.TYPES[self.profile_scoring_type]))
+        return res
+
+    name = fields.Char('Name', required=True, track_visibility='onchange')
+    rule_type = fields.Selection([
+        ('score', 'Scoring'),
+        ('active', 'Archive'),
+        ('unlink', 'Delete')], default='score', required=True)
+    profile_scoring_type = fields.Selection([
+        ('contact_type', 'Contact Type'), ('title', 'Title'),
+        ('sales_channels', 'Sales Channels'),
+        ('contact_status', 'Contact Status'),
+        ('country', 'Country')], default='contact_type', required=True)
+    event_based = fields.Boolean('Event-based rule')
+    running = fields.Boolean('Active', default=True)
+    type_scoring_ids = fields.One2many(
+        'contacts.type.scoring', 'score_id', 'Profile Scoring')
+    status_scoring_ids = fields.One2many(
+        'contacts.status.scoring', 'score_id', 'Profile Scoring')
+    channels_scoring_ids = fields.One2many(
+        'crm.team.scoring', 'score_id', 'Profile Scoring')
+    title_scoring_ids = fields.One2many(
+        'partner.title.scoring', 'score_id', 'Profile Scoring')
+    country_scoring_ids = fields.One2many(
+        'country.scoring', 'score_id', 'Profile Scoring')
+
+    @api.multi
+    def check_duplication(self, line, field_id, ids_list):
+        val = getattr(line, field_id).id
+        if val not in ids_list:
+            ids_list.append(val)
+        else:
+            raise ValidationError(_("You can not add same type twice!"))
+
+    @api.multi
+    @api.constrains(
+        'type_scoring_ids', 'status_scoring_ids',
+        'channels_scoring_ids', 'title_scoring_ids',
+        'country_scoring_ids')
+    def _check_score(self):
+        ids_list = []
+        res = {
+            'contact_type': self.type_scoring_ids,
+            'title': self.title_scoring_ids,
+            'sales_channels': self.channels_scoring_ids,
+            'contact_status': self.status_scoring_ids,
+            'country': self.country_scoring_ids
+        }
+
+        fields = [
+            'type_id', 'status_id', 'team_id', 'partner_title_id', 'country_id'
+        ]
+
+        for line in res[self.profile_scoring_type]:
+            if line.score < 0.0 or line.score > 10.0:
+                raise ValidationError(_(
+                    "Invalid Score!\n\nScore must be in range 1 - 10."))
+
+            for field in fields:
+                if hasattr(line, field):
+                    self.check_duplication(line, field, ids_list)

addons/crm_scoring/tests/lead_scoring.py
@@ -1,0 +1,54 @@
+# Part of Flectra. See LICENSE file for full copyright and licensing details.
+
+from flectra.tests.common import TransactionCase
+
+
+class LeadScoringCases(TransactionCase):
+
+    def setUp(self):
+        super(LeadScoringCases, self).setUp()
+        self.lead = self.env['crm.lead']
+        self.lead_score = self.env['crm.lead.score']
+        self.title = self.env.ref('base.res_partner_title_doctor')
+        self.country = self.env.ref('base.ar')
+        self.channel = self.env.ref('sales_team.team_sales_department')
+        self.contact_type = self.env.ref(
+            'crm_scoring.crm_score_lead_contact_type1')
+        self.contact_status = self.env.ref(
+            'crm_scoring.crm_score_lead_profile_contact_status7')
+
+    def test_01_create_lead_scoring(self):
+        '''
+            Create Lead score
+        '''
+        line_vals = {
+            'name': 'Doctor',
+            'partner_title_id': self.title.id,
+            'score': 10.0
+        }
+
+        scoring_id = self.lead_score.create({
+            'name': 'Score Test 1',
+            'rule_type': 'score',
+            'profile_scoring_type': 'title',
+            'event_based': True,
+            'running': True,
+            'title_scoring_ids': [(0, 0, line_vals)]
+        })
+
+        scoring_id.write({'profile_scoring_type': 'country'})
+
+    def test_02_create_leads(self):
+        '''
+            Create Leads
+        '''
+        self.lead.create({
+            'name': 'Lead Test',
+            'contact_name': 'Dom Toretto',
+            'title': self.title.id,
+            'team_id': self.channel.id,
+            'contact_type_id': self.contact_type.id,
+            'contact_status_id': self.contact_status.id,
+            'prospective_lead': True,
+            'priority': 2,
+        })

gagaboy/odoo10_plus

addons/crm_scoring/models/crm_lead_score.py
@@ -1,0 +1,104 @@
+# Part of Flectra. See LICENSE file for full copyright and licensing details.
+
+from flectra import api, fields, models, _
+from flectra.exceptions import ValidationError
+
+
+class CRMLeadScore(models.Model):
+    _name = "crm.lead.score"
+    _inherit = ['mail.thread']
+    _description = "Lead Soring Rule"
+
+    TYPES = {
+        'contact_type': 'Contact Type',
+        'title': 'Title',
+        'sales_channels': 'Sales Channels',
+        'contact_status': 'Contact Status',
+        'country': 'Country',
+    }
+
+    @api.model
+    def create(self, vals):
+        res = super(CRMLeadScore, self).create(vals)
+        same_records = res.search([
+            ('id', '!=', res.id),
+            ('profile_scoring_type', '=', res.profile_scoring_type)
+        ])
+        if same_records:
+            raise ValidationError(_(
+                "There's already one rule defined for '%s' type!") %
+                str(self.TYPES[res.profile_scoring_type]))
+        return res
+
+    @api.multi
+    def write(self, vals):
+        res = super(CRMLeadScore, self).write(vals)
+        if 'profile_scoring_type' in vals:
+            same_records = self.search([
+                ('id', '!=', self.id),
+                ('profile_scoring_type', '=', vals['profile_scoring_type'])
+            ])
+            if same_records:
+                raise ValidationError(_(
+                    "There's already one rule defined for '%s' type!") %
+                    str(self.TYPES[self.profile_scoring_type]))
+        return res
+
+    name = fields.Char('Name', required=True, track_visibility='onchange')
+    score_rule_type = fields.Selection([
+        ('scoring', 'Scoring'),
+        ('archive', 'Archive'),
+        ('delete', 'Delete')], default='scoring', required=True)
+    profile_scoring_type = fields.Selection([
+        ('contact_type', 'Contact Type'), ('title', 'Title'),
+        ('sales_channels', 'Sales Channels'),
+        ('contact_status', 'Contact Status'),
+        ('country', 'Country')], default='contact_type', required=True)
+    is_event_based = fields.Boolean(string="Based on Event?")
+    is_running = fields.Boolean(string="Currently Running?", default=True)
+    type_scoring_ids = fields.One2many(
+        'contacts.type.scoring', 'score_id', 'Profile Scoring')
+    status_scoring_ids = fields.One2many(
+        'contacts.status.scoring', 'score_id', 'Profile Scoring')
+    channels_scoring_ids = fields.One2many(
+        'crm.team.scoring', 'score_id', 'Profile Scoring')
+    title_scoring_ids = fields.One2many(
+        'partner.title.scoring', 'score_id', 'Profile Scoring')
+    country_scoring_ids = fields.One2many(
+        'country.scoring', 'score_id', 'Profile Scoring')
+
+    @api.multi
+    def check_duplication(self, line, field_id, ids_list):
+        val = getattr(line, field_id).id
+        if val not in ids_list:
+            ids_list.append(val)
+        else:
+            raise ValidationError(_("You can not add same type twice!"))
+
+    @api.multi
+    @api.constrains(
+        'type_scoring_ids', 'status_scoring_ids',
+        'channels_scoring_ids', 'title_scoring_ids',
+        'country_scoring_ids')
+    def _check_score(self):
+        ids_list = []
+        res = {
+            'contact_type': self.type_scoring_ids,
+            'title': self.title_scoring_ids,
+            'sales_channels': self.channels_scoring_ids,
+            'contact_status': self.status_scoring_ids,
+            'country': self.country_scoring_ids
+        }
+
+        fields = [
+            'type_id', 'status_id', 'team_id', 'partner_title_id', 'country_id'
+        ]
+
+        for line in res[self.profile_scoring_type]:
+            if line.score < 0.0 or line.score > 10.0:
+                raise ValidationError(_(
+                    "Invalid Score!\n\nScore must be in range 1 - 10."))
+
+            for field in fields:
+                if hasattr(line, field):
+                    self.check_duplication(line, field, ids_list)

addons/crm_scoring/tests/lead_scoring.py
@@ -1,0 +1,54 @@
+# Part of Flectra. See LICENSE file for full copyright and licensing details.
+
+from flectra.tests.common import TransactionCase
+
+
+class LeadScoringCases(TransactionCase):
+
+    def setUp(self):
+        super(LeadScoringCases, self).setUp()
+        self.lead = self.env['crm.lead']
+        self.lead_score = self.env['crm.lead.score']
+        self.title = self.env.ref('base.res_partner_title_doctor')
+        self.country = self.env.ref('base.ar')
+        self.channel = self.env.ref('sales_team.team_sales_department')
+        self.contact_type = self.env.ref(
+            'crm_scoring.crm_score_lead_contact_type1')
+        self.contact_status = self.env.ref(
+            'crm_scoring.crm_score_lead_profile_contact_status7')
+
+    def test_01_create_lead_scoring(self):
+        '''
+            Create Lead score
+        '''
+        line_vals = {
+            'name': 'Doctor',
+            'partner_title_id': self.title.id,
+            'score': 10.0
+        }
+
+        scoring_id = self.lead_score.create({
+            'name': 'Score Test 1',
+            'score_rule_type': 'scoring',
+            'profile_scoring_type': 'title',
+            'is_event_based': True,
+            'is_running': True,
+            'title_scoring_ids': [(0, 0, line_vals)]
+        })
+
+        scoring_id.write({'profile_scoring_type': 'country'})
+
+    def test_02_create_leads(self):
+        '''
+            Create Leads
+        '''
+        self.lead.create({
+            'name': 'Lead Test',
+            'contact_name': 'Dom Toretto',
+            'title': self.title.id,
+            'team_id': self.channel.id,
+            'contact_type_id': self.contact_type.id,
+            'contact_status_id': self.contact_status.id,
+            'prospective_lead': True,
+            'priority': 2,
+        })